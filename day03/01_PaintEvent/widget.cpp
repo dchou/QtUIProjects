aed583acdf5ea23c889308f5901a96abc97b9c92
David Chou <dchou@iscreen.com>
2019-08-16 11:14:00 +0800

    3.4 - 简单绘图

--- day03/01_PaintEvent/widget.cpp
+++ day03/01_PaintEvent/widget.cpp
@@ -1,6 +1,8 @@
 #include "widget.h"
 #include "ui_widget.h"
 #include <QPainter>
+#include <QPen>
+#include <QBrush>
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
@@ -27,12 +29,46 @@ void Widget::paintEvent(QPaintEvent *)
     //  p.drawxxx();
     //  p.end();
 
-    QPainter p(this);
-    p.drawPixmap(0, 0, width(), height(), QPixmap("://Image/sunny.png"));
+    //OK QPainter p(this);
+    //OK p.drawPixmap(0, 0, width(), height(), QPixmap("://Image/sunny.png"));
+    //OK p.drawPixmap(rect(), QPixmap("://Image/sunny.png"));
 
     //OK
     //QPainter p;  //創建畫家對象
     //p.begin(this); //指定當前窗口為繪圖設備
     //p.drawPixmap(0, 0, width(), height(), QPixmap("://Image/Sunny.jpg"));
     //p.end();
+
+    QPainter p;    //創建畫家對象
+    p.begin(this);
+
+    //定義畫筆
+    QPen pen;
+    pen.setWidth(5);
+    //OK pen.setColor(Qt::red);      //設置顏色
+    pen.setColor(QColor(14, 9, 234)); //rgb設置顏色
+    pen.setStyle(Qt::DashLine);      //設置風格
+
+    //把畫筆交給畫家
+    p.setPen(pen);
+
+    //畫直線
+    p.drawLine(50, 50, 150, 50);
+    p.drawLine(50, 50, 50, 150);
+
+    //創建畫刷對象
+    QBrush brush;
+    brush.setColor(Qt::red);      //設置顏色
+    brush.setStyle(Qt::Dense1Pattern);
+
+    //把畫刷交給畫家
+    p.setBrush(brush);
+
+    //畫矩形
+    p.drawRect(150, 150, 100, 50);
+
+    //畫圓形/橢圓形
+    p.drawEllipse(QPoint(150, 150), 50, 25);
+
+    p.end();
 }
