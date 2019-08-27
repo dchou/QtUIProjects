26a5573b86eba15a50b07506eb880dad98979ecc
David Chou <david.chou@iscreen.com>
2019-08-27 20:14:36 +0800

    5.9 new Thread usage

---
+++ day05/02_ThreadPro/mythread.cpp
@@ -0,0 +1,30 @@
+#include "mythread.h"
+#include <QThread>
+#include <QDebug>
+
+MyThread::MyThread(QObject *parent) : QObject(parent)
+{
+    isStop = false;
+}
+
+void MyThread::myTimeout()
+{
+    while( !isStop )
+    {
+        QThread::sleep(1);
+        emit mySignal();
+        //QMessageBox::aboutQt(NULL);
+
+        qDebug() << "子线程号：" << QThread::currentThread();
+
+        /*DC if(isStop)
+        {
+            break;
+        }*/
+    }
+}
+
+void MyThread::setFlag(bool flag)
+{
+    isStop = flag;
+}
