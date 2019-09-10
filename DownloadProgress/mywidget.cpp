8065b402e250525a5ea3b42acd846522b6f234cb
David Chou <dchou@iscreen.com>
2019-09-10 21:16:48 +0800

    DownloadProgress v2

--- DownloadProgress/mywidget.cpp
+++ DownloadProgress/mywidget.cpp
@@ -1,8 +1,11 @@
+// reference: https://www.cnblogs.com/li-peng/p/3656613.html
 #include "mywidget.h"
 #include "ui_mywidget.h"
 #include <QNetworkAccessManager>
 #include <QNetworkRequest>
 #include <QNetworkReply>
+#include <QFileInfo>
+#include <QDir>
 
 MyWidget::MyWidget(QWidget *parent) :
     QWidget(parent),
@@ -24,26 +27,63 @@ void MyWidget::on_pushButton_clicked()
     QNetworkRequest request;
     request.setUrl(QUrl(URL));
 
-    QNetworkReply *reply = mgr.get(request);  //自动产生新的線程
+    mgr = new QNetworkAccessManager(this);
+    reply = mgr->get(request);  //自动产生新的線程
 
     bool rc1 = connect(reply, SIGNAL(finished()), this, SLOT(onDownloadFinished()));
     qDebug() << "rc1: " << rc1;
     bool rc2 = connect(reply, SIGNAL(downloadProgress(qint64, qint64)),
                           this, SLOT(onDownloadProgress(qint64, qint64)));
     qDebug() << "rc2: " << rc2;
+    bool rc3 = connect(reply,SIGNAL(readyRead()),this,SLOT(onReadyRead()));
+    qDebug() << "rc3: " << rc3;
+
     qDebug() << "start download image:";
 }
 
 void MyWidget::onDownloadFinished()
 {
     qDebug() << "onDownloadFinished()";
+    file->flush();
+    file->close();
 }
 
 void MyWidget::onDownloadProgress(qint64 bytesReceived, qint64 bytesTotal)
 {
-    float percent_float = float(bytesReceived) / float(bytesTotal);
-    int precent_int = percent_float * 100;
-    qDebug() << "onDownloadProgress() " << percent_float;
-    ui->progressBar->setValue(precent_int);
+    int percent = float(bytesReceived) / float(bytesTotal) * 100;
+    qDebug() << "onDownloadProgress() " << percent;
+    ui->progressBar->setValue(percent);
+}
 
+void MyWidget::onReadyRead()
+{
+    qDebug() << "onReadyRead() ";
+
+    QFileInfo fileinfo(reply->url().path());
+    QString thefileName = fileinfo.fileName().isEmpty()?"index.html":fileinfo.fileName();
+
+    QString thePath = "/tmp/download/";
+    //qDebug()<<thePath;
+    QDir createfile;
+    bool exist = createfile.exists(thePath);
+    if(!exist)
+    {
+        createfile.mkpath(thePath);
+    }
+    thePath += thefileName;
+    qDebug() << "onReadyRead(): " << thePath;
+    file = new QFile(thePath);
+
+//    if(file->open(QIODevice::Append))
+//    {
+//        file->write(managerReply->readAll());
+//        file->flush();
+//        file->close();
+//    }
+    if(file->open(QIODevice::Append))
+    {
+        file->write(reply->readAll());
+        file->flush();
+    }
+    //delete file;
 }
--- DownloadProgress/mywidget.h
+++ DownloadProgress/mywidget.h
@@ -3,6 +3,8 @@
 
 #include <QWidget>
 #include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QFile>
 
 namespace Ui {
 class MyWidget;
@@ -20,12 +22,15 @@ private slots:
     void on_pushButton_clicked();
     void onDownloadFinished();
     void onDownloadProgress(qint64 bytesReceived, qint64 bytesTotal);
+    void onReadyRead();
 
 private:
-    Ui::MyWidget *ui;
-    QNetworkAccessManager mgr;
     //QString URL = "http://cdn.atgamescloud.com/retro-apps/production/AppVersion/ha8800_test/10/ha8800_0.9.100.img?ttl=1568108934&sig=b5a5bfe3d5ad62305bba9ce531695742";
     QString URL = "https://mirrors.tuna.tsinghua.edu.cn/qt/archive/online_installers/3.1/qt-unified-mac-x64-3.1.1-online.dmg";
+    Ui::MyWidget *ui;
+    QNetworkAccessManager *mgr;
+    QNetworkReply *reply;
+    QFile *file;
 };
 
 #endif // MYWIDGET_H
